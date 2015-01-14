f6211375024b8361eff013a2a2b0771c30dbbcf4
C-B-Fairchild <chris.fairchild@gmail.com>
2015-01-14 15:44:11 -0500

    added simple form gem

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'sass-rails', '~> 4.0.0'
 
 gem 'bootstrap-sass', '~> 3.1.1'
 
+gem 'simple_form'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,4 @@ module ApplicationHelper
   def my_name
     "Chris Fairchild"
   end
-
-  def form_group_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-errors'
-    else
-      content_tag :div, capture(&block), class: 'form-group'
-    end
-  end
 end
